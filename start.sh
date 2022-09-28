64f615a0173906527beb331636734f2178646263
Chandra Sagar <chsagar@gmail.com>
2022-09-28 12:37:45 +0530

    env variable section updated

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 npx postgraphile --cors \
-  -c postgres://cakestore_postgraphile:cakestore_secret@${{PGHOST}}:${{PGPORT}}/railway \
+  -c postgres://cakestore_postgraphile:cakestore_secret@${PGHOST}:${PGPORT}/railway \
   -n 0.0.0.0 \
   --schema cakestore \
   --append-plugins @graphile-contrib/pg-simplify-inflector,postgraphile-plugin-connection-filter,@graphile/postgis \
